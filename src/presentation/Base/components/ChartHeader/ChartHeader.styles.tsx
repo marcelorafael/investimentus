a6360c320d77d990d406e178781f2306228a832c
marcelorafael <marcelorafael23@hotmail.com>
2021-07-24 19:50:44 -0300

    feat: base CHatHeader created

--- src/presentation/Base/components/ChartHeader/ChartHeader.styles.tsx
+++ src/presentation/Base/components/ChartHeader/ChartHeader.styles.tsx
@@ -1,8 +1,13 @@
 import styled from 'styled-components/native';
-import LinearGradient from 'react-native-linear-gradient';
+// import LinearGradient from 'react-native-linear-gradient';
 
 export const Container = styled.View`
+
+`;
+
+export const Header = styled.View`
 background-color: ${props => props?.theme?.COLORS?.greenFocused ?? '#218c74'};
 width: 100%;
 height: 250px;
+justify-content: flex-end;
 `;
--- src/presentation/Base/components/ChartHeader/ChartHeader.tsx
+++ src/presentation/Base/components/ChartHeader/ChartHeader.tsx
@@ -1,9 +1,51 @@
 import React from 'react'
-import { Container } from './ChartHeader.styles'
+import { Container, Header, } from './ChartHeader.styles';
+import { Dimensions } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 const ChartHeader: React.FC = () => {
+  const data = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43]
+      }
+    ]
+  };
+
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+  const screenWidth = Dimensions.get("window").width;
+
   return (
-    <Container></Container>
+    <Container>
+      <Header>
+        <BarChart
+          style={{alignItems:'center'}}
+          data={data}
+          width={screenWidth}
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        />
+      </Header>
+    </Container>
   )
 }
 const ChartHeaderMemo = React.memo(ChartHeader);
