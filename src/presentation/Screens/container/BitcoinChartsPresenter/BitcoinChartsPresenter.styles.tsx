90daab5e59604dd172e222ab0e3fae00f2a4fa19
marcelorafael <marcelorafael23@hotmail.com>
2021-07-30 17:49:12 -0300

    feat: screen Bitcoin completed

--- src/presentation/Screens/container/BitcoinChartsPresenter/BitcoinChartsPresenter.styles.tsx
+++ src/presentation/Screens/container/BitcoinChartsPresenter/BitcoinChartsPresenter.styles.tsx
@@ -7,6 +7,37 @@ flex: 1;
 export const Title = styled.Text`
 ${({theme}) => theme?.FONTS?.oswald ?? {}}
 color: ${({theme}) => theme?.COLORS?.white ?? '#fff'};
+font-size: 15px;
+margin-top: 8px;
+text-align: center;
 `;
 
-export const CenterView = styled.View``;
+export const TextValues = styled.Text`
+${({theme}) => theme?.FONTS?.robotoThin ?? {}}
+color: ${({theme}) => theme?.COLORS?.white ?? '#fff'};
+margin-top: 8px;
+`;
+
+export const CenterView = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false
+})`
+flex: 1;
+margin-bottom: 15px;
+`;
+
+export const ContainerCrypto = styled.View`
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 8px;
+
+height: 80px;
+`;
+
+export const ContainerValues = styled.View`
+flex: 1;
+justify-content: space-between;
+
+border-top-width: 1px;
+border-top-color: #FFF;
+`;
--- src/presentation/Screens/container/BitcoinChartsPresenter/BitcoinChartsPresenter.tsx
+++ src/presentation/Screens/container/BitcoinChartsPresenter/BitcoinChartsPresenter.tsx
@@ -1,23 +1,59 @@
 import React, { useState, useEffect }  from 'react'
-import { Container, CenterView, Title } from './BitcoinChartsPresenter.styles';
+import { 
+  Container, 
+  CenterView, 
+  Title,
+  TextValues,
+  ContainerCrypto,
+  ContainerValues,
+} from './BitcoinChartsPresenter.styles';
 import { BitcoinCharts } from '../../components/BitcoinCharts';
 import Stocks from '../../../../services/axios/GetDatas/Stocks';
+import { View } from 'react-native';
 
 const BitcoinChartsPresenter: React.FC = () => {
+  const[cyrptos, useCyrptos] = useState([]);
 
   useEffect(() => {
     async function handleCoin() {
       const response = await Stocks('/?key=34664f77');
 
-      console.log(response.bitcoin)
+      const keys: any = Object.entries(response);
+      const adapter: any = Object.values(keys[3][1]);
+      // adapter[1][1]
+
+      useCyrptos(adapter)
+
+      console.log(adapter.map(item => item).sort())
     }
     handleCoin();
-  },[])
+  },[]);
+
   return (
     <Container style={{flex: 1}}>
       <BitcoinCharts title="Crypto Moedas">
         <CenterView>
-          <Title>Bitcoin</Title>
+          {cyrptos.map((item: any) => (
+            <ContainerValues>
+              <Title>{item.name === 'Blockchain.info' ? 'Bitcoin' : item.name}</Title>
+              <ContainerCrypto>
+                <View>
+                  <TextValues>Compra</TextValues>
+                  <TextValues>{typeof item.buy === 'undefined' ? '-' : item.buy}</TextValues>
+                </View>
+
+                <View>
+                  <TextValues>venda</TextValues>
+                  <TextValues>{typeof item.sell === 'undefined' ? '--' : item.sell}</TextValues>
+                </View>
+
+                <View>
+                  <TextValues>Variação</TextValues>
+                  <TextValues>{item.variation}</TextValues>
+                </View>
+              </ContainerCrypto>
+            </ContainerValues>
+          ))}
         </CenterView>
       </BitcoinCharts>
     </Container>
