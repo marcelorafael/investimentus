012ae19f0fa50a8c5c8fddfe6251efb332f97ce1
marcelorafael <marcelorafael23@hotmail.com>
2021-08-15 18:06:02 -0300

    feat: change file test to spec

--- src/presentation/Screens/container/BitcoinChartsPresenter/BitcoinChartsPresenter.spec.tsx
+++ src/presentation/Screens/container/BitcoinChartsPresenter/BitcoinChartsPresenter.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { render } from '@testing-library/react-native'
+
 import { BitcoinChartsPresenter } from './BitcoinChartsPresenter'
 import { renderWithProviders } from '../../../tests/utils'
 import 'setimmediate'
